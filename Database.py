855b61209afd4c089463c82cf6a1da4f492e2c3d
DURRAINk <durrainpathan123@gmail.com>
2025-08-17 16:12:47 +0530

    changed driver

--- Database.py
+++ Database.py
@@ -5,7 +5,7 @@ server_name = "LAPTOP-O4M1T2S4\\SQLEXPRESS"
 database_name = "Food_Management"
 connection_string = (
     f"mssql+pyodbc://{server_name}/{database_name}"
-    "?driver=SQL+Server"
+    "?driver=ODBC+Driver+17+for+SQL+Server"
     "&trusted_connection=yes"
 )
 engine = create_engine(connection_string)
